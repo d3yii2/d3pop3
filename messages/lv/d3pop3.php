7de87aaf0ce50053dde4eb677cab5842c2359769
Uldis Nelsons <uldisnelsons@gmail.com>
2019-01-03 15:26:48 +0200

    Tulkošana

--- messages/lv/d3pop3.php
+++ messages/lv/d3pop3.php
@@ -1,4 +1,5 @@
 <?php
 return [
     'Email: {email} has already been taken for type: {type}' => 'Šai epasta adresei: {email} jau eksistē konfigurācija priekš : {type}',
+    'Mailbox Name' => 'Epasts'
 ];
