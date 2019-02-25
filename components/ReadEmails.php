c221819ed04661119440efc7ff33ed4ad8289fb8
uldisn <uldisnelsons@gmail.com>
2019-02-25 08:37:27 +0200

    In command ReadEmails fixed try and catch implementation
    Signed-off-by: uldisn <uldisnelsons@gmail.com>

--- components/ReadEmails.php
+++ components/ReadEmails.php
@@ -11,19 +11,21 @@ use unyii2\imap\ImapConnection;
 use yii\helpers\FileHelper;
 use yii\helpers\VarDumper;
 
-class ReadEmails {
+class ReadEmails
+{
 
     /**
      * @param EmailContainerInerface $cc
      * @param $containerClass
      * @return bool
      */
-    public static function readImap(EmailContainerInerface $cc, $containerClass) {
-        
+    public static function readImap(EmailContainerInerface $cc, $containerClass)
+    {
+
         $error = false;
-        
+
         $tempDirectory = Yii::getAlias('@runtime/temp');
-        
+
         while ($cc->featchData()) {
 
             $imapConnection = new ImapConnection();
@@ -31,9 +33,9 @@ class ReadEmails {
             $imapConnection->imapLogin = $cc->getUserName();
             $imapConnection->imapPassword = $cc->getPassword();
             $imapConnection->serverEncoding = 'utf-8'; // utf-8 default.
-            $imapConnection->attachmentsDir =  $tempDirectory;
+            $imapConnection->attachmentsDir = $tempDirectory;
 
-            echo 'Connect to ' . $cc->getImapPath(). ' userName: ' . $cc->getUserName() .' (id='.$cc->getId().')' .PHP_EOL;
+            echo 'Connect to ' . $cc->getImapPath() . ' userName: ' . $cc->getUserName() . ' (id=' . $cc->getId() . ')' . PHP_EOL;
 
             /**
              * connect to IMAP
@@ -46,30 +48,38 @@ class ReadEmails {
                 $mailbox->readMailParts = false;
 
                 $mailsIds = $mailbox->searchMailbox('ALL');
-                if(!$mailsIds) {
+                if (!$mailsIds) {
                     echo 'Mailbox is empty' . PHP_EOL;
                     continue;
                 }
 
                 echo 'Messages count:' . count($mailsIds) . PHP_EOL;
-                foreach ($mailsIds as $i => $mailId) {
+            } catch (\Exception $e) {
+                $message = 'Container class: ' . $containerClass . '; ; Error: ' . $e->getMessage();
+                echo $message . PHP_EOL;
+                \Yii::error($message);
+                \Yii::error(VarDumper::export($e->getTrace()));
+                Action::error($cc->getId(), $message);
+                continue;
+            }
+            foreach ($mailsIds as $i => $mailId) {
 
 
+                $msg = $mailbox->getMail($mailId);
+                echo $i . ' Subject:' . $msg->subject . PHP_EOL;
+                echo $i . ' Date:' . $msg->date . PHP_EOL;
+                echo $i . ' MessageId:' . $msg->messageId . PHP_EOL;
 
-                    $msg = $mailbox->getMail($mailId);
-                    echo $i . ' Subject:' . $msg->subject . PHP_EOL;
-                    echo $i . ' Date:' . $msg->date . PHP_EOL;
-                    echo $i . ' MessageId:' . $msg->messageId . PHP_EOL;
+                if (D3pop3Email::findOne(['email_id' => $msg->messageId])) {
+                    echo $i . ' Message already loaded' . PHP_EOL;
+                    continue;
+                }
 
-                    if(D3pop3Email::findOne(['email_id' => $msg->messageId])){
-                        echo $i . ' Message already loaded' . PHP_EOL;
-                        continue;
-                    }
-
+                $transaction = \Yii::$app->db->beginTransaction();
+                try {
                     /**
                      * load attachments and bodies
                      */
-                    $transaction = \Yii::$app->db->beginTransaction();
                     $msg = $mailbox->getMailParts($msg);
 
                     $d3mail = new D3Mail();
@@ -78,27 +88,26 @@ class ReadEmails {
                         ->setBodyPlain($msg->textPlain)
                         ->setBodyHtml($msg->textHtml)
                         ->setFromName($msg->fromName)
-                        ->setFromEmail( $msg->fromAddress)
+                        ->setFromEmail($msg->fromAddress)
                         ->setEmailContainerClass($containerClass)
                         ->setEmailContainerId($cc->getId())
-                        ->setEmailId($msg->id)
-                        ;
+                        ->setEmailId($msg->id);
 
-                    if($containerClass === SettingEmailContainer::class){
-                        if($setting = D3pop3ConnectingSettings::find()->andWhere(['id' => $cc->getId()])->one()){
+                    if ($containerClass === SettingEmailContainer::class) {
+                        if ($setting = D3pop3ConnectingSettings::find()->andWhere(['id' => $cc->getId()])->one()) {
                             $d3mail->addSendReceiveToInCompany($setting->sys_company_id);
                         }
                     }
 
-                    foreach ($msg->to as $toEmail => $toName){
+                    foreach ($msg->to as $toEmail => $toName) {
                         $d3mail->addAddressTo($toEmail, $toName);
                     }
 
-                    foreach ($msg->cc as $ccEmail => $ccName){
+                    foreach ($msg->cc as $ccEmail => $ccName) {
                         $d3mail->addAddressTo($ccEmail, $ccName);
                     }
 
-                    foreach ($msg->replyTo as $rtEmail => $rtName){
+                    foreach ($msg->replyTo as $rtEmail => $rtName) {
                         $d3mail->addAddressTo($rtEmail, $rtName);
                     }
 
@@ -112,26 +121,25 @@ class ReadEmails {
                     $d3mail->save();
                     $transaction->commit();
                     echo PHP_EOL;
+                } catch (\Exception $e) {
+                    $transaction->rollBack();
+                    $message = 'Container class: ' . $containerClass . '; Error: ' . $e->getMessage();
+                    echo $message . PHP_EOL;
+                    \Yii::error($message);
+                    \Yii::error(VarDumper::export($e->getTrace()));
+                    Action::error($cc->getId(), $message);
+                    continue;
                 }
-
-
-            } catch (\Exception $e) {
-                $transaction->rollBack();
-                $message = 'Container class: ' . $containerClass . '; ; Error: ' . $e->getMessage();
-                echo $message . PHP_EOL;
-                \Yii::error($message);
-                \Yii::error( VarDumper::export($e->getTrace()));
-                Action::error($cc->getId(),$message);
-                continue;
             }
 
+
         }
-        
+
         /**
          * remove all attachment files
          */
         $files = FileHelper::findFiles($tempDirectory);
-        foreach($files as $f){
+        foreach ($files as $f) {
             unlink($f);
         }
         return !$error;
