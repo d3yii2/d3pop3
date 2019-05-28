01e1bdf5af13322293392043a7058baade52afc3
Lauris Mierkalns <latpages@gmail.com>
2019-05-28 14:29:55 +0300

    D3pop3SendReceiv find() PHPDoc & return tips

--- models/D3pop3SendReceiv.php
+++ models/D3pop3SendReceiv.php
@@ -3,13 +3,17 @@
 namespace d3yii2\d3pop3\models;
 
 use \d3yii2\d3pop3\models\base\D3pop3SendReceiv as BaseD3pop3SendReceiv;
+use yii\db\ActiveQuery;
 
 /**
  * This is the model class for table "d3pop3_send_receiv".
  */
 class D3pop3SendReceiv extends BaseD3pop3SendReceiv
 {
-    public static function find()
+    /**
+     * @return ActiveQuery
+     */
+    public static function find(): ActiveQuery
     {
         $query = parent::find();
         $query->andWhere(['company_id' => \Yii::$app->SysCmp->getActiveCompanyId()]);
