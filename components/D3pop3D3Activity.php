2d502f2d1a51fcd1e01af5ab5f0ff2df3db23ed7
vairis <vairis@webhaus.lv>
2022-01-21 04:03:05 +0200

    - d3pop3d3activity compontent

---
+++ components/D3pop3D3Activity.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace d3yii2\d3pop3\components;
+
+
+use d3yii2\d3activity\components\ActivityRecord;
+use d3yii2\d3activity\components\ModelActivityInterface;
+use d3yii2\d3pop3\models\D3pop3Email;
+
+class D3pop3D3Activity implements ModelActivityInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public static function findByIdList(
+        array $idList,
+        array $filter = [],
+        array $additionalFields = []
+    ): array
+    {
+        if (!$idList) {
+            return [];
+        }
+        $list = [];
+        $where = [
+            'id' => $idList
+        ];
+
+        foreach (D3pop3Email::findAll($where) as $email) {
+            $record = new ActivityRecord();
+            $record->recordId = $email->id;
+            $record->label = $email->from . ' --- ' .$email->subject;
+            $record->url = [
+                '/d3emails/email/view',
+                'id' => $email->id,
+                'box' => 'inbox'
+            ];
+
+            $list[] = $record;
+        }
+
+        return $list;
+    }
+
+    public static function findModel(int $id)
+    {
+        return D3pop3Email::findModel($id);
+    }
+}
