e7b6457b5111d356f75733318b60455d47b38276
uldisn <uldisnelsons@gmail.com>
2023-05-09 21:20:44 +0300

    Modelim D3pop3SendReceiv find() uzlikts D3ActiveQuery
    Signed-off-by: uldisn <uldisnelsons@gmail.com>

--- models/D3pop3SendReceiv.php
+++ models/D3pop3SendReceiv.php
@@ -2,7 +2,9 @@
 
 namespace d3yii2\d3pop3\models;
 
-use \d3yii2\d3pop3\models\base\D3pop3SendReceiv as BaseD3pop3SendReceiv;
+use d3system\yii2\db\D3ActiveQuery;
+use d3yii2\d3pop3\models\base\D3pop3SendReceiv as BaseD3pop3SendReceiv;
+use Yii;
 use yii\db\ActiveQuery;
 
 /**
@@ -15,8 +17,9 @@ class D3pop3SendReceiv extends BaseD3pop3SendReceiv
      */
     public static function find(): ActiveQuery
     {
-        $query = parent::find();
-        $query->andWhere(['company_id' => \Yii::$app->SysCmp->getActiveCompanyId()]);
+        $query = new D3ActiveQuery(static::class);
+        //$query = parent::find();
+        $query->andWhere(['company_id' => Yii::$app->SysCmp->getActiveCompanyId()]);
 
         return $query;
     }
